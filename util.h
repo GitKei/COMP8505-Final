814f08d9a1383a44cc9d980d7148a856d4f99f1b
Steve <kythyx@gmail.com>
2011-12-04 16:04:42 -0800

    commenting

--- util.h
+++ util.h
@@ -4,12 +4,13 @@
 /*
 SOURCE FILE: util.h
 
-PROGRAM: Backdoor - A simple two in one backdoor client/server using libpcap
+PROGRAM: Backdoor - A covert backdoor client/server using libpcap and raw sockets.
 
-DATE: October 23, 2011
+DATE: November 29, 2011
 
 REVISIONS:
 	1.0 - October 23
+	1.1 - November 29, 2011
 
 DESIGNERS:
 	Santana Mach and Steve Stanisic
